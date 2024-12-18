f6fb46993a40f4c425ab71ec824450c2008889ec
caleb05w <99751567+caleb05w@users.noreply.github.com>
2024-12-18 01:48:25 -0800

    changed pathing for navbar and footer

--- app/layout.js
+++ app/layout.js
@@ -2,8 +2,9 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 
 //Components
-import Navbar from "../components/Navbar";
-import Footer from "../components/Footer";
+
+import Navbar from "../Components/Navbar";
+import Navbar from "../Components/Footer";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
