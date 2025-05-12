d51e006ec2915a92ca0acba08f8b3326d6300df1
manggo-cd <danielzhou.nc@gmail.com>
2025-05-12 11:52:41 -0700

    Removed people from about us team page and changed role descriptions

--- app/about/page.js
+++ app/about/page.js
@@ -194,78 +194,37 @@ function page() {
             />
 
             <AboutProfile
-              Profile="/static/images/newNaia.png"
-              Name="Naia Wong"
-              Role="Director of Operations"
-              Linkedin="https://www.linkedin.com/in/naia-wong/"
-            />
-
-            <AboutProfile
-              Profile="/static/images/newGailza.png"
-              Name="Gailza Wijaya"
-              Role="VP Sales"
-              Linkedin="https://www.linkedin.com/in/gailzaaridinawijaya/"
-            />
-
-            <AboutProfile
               Profile="/static/images/newDarren.png"
               Name="Darren Lau"
-              Role="Director of Business Development"
+              Role="Head of Operations"
               Linkedin="https://www.linkedin.com/in/darrennlau/"
             />
 
             <AboutProfile
               Profile="/static/images/newArianna.png"
               Name="Arianna Ha"
-              Role="VP Marketing"
+              Role="Head of Finance"
               Linkedin="https://www.linkedin.com/in/arianna-ha/"
             />
 
             <AboutProfile
               Profile="/static/images/newBrandon.png"
               Name="Brandon Sun "
-              Role="Sales Lead"
+              Role="Head of Sales & Partnerships"
               Linkedin="https://www.linkedin.com/in/brandnsun/"
-            />
-
-            <AboutProfile
-              Profile="/static/images/newVinay.png"
-              Name="Vinay Aery"
-              Role="Finance Coordinator"
-              Linkedin="https://www.linkedin.com/in/vinayaery/"
-            />
-            <AboutProfile
-              Profile="/static/images/newMichael.png"
-              Name="Michael Gudz"
-              Role="Sales Coordinator"
-              Linkedin="https://www.linkedin.com/in/michaelgudz123/"
-            />
-
-            <AboutProfile
-              Profile="/static/images/newJessica.png"
-              Name="Jessica Tandibrata"
-              Role="Events Coordinator"
-              Linkedin="https://www.linkedin.com/in/jessicatandibrata/"
-            />
-
-            <AboutProfile
-              Profile="/static/images/newLucy.png"
-              Name="Lucy Liu"
-              Role="Operations Coordinator"
-              Linkedin="https://www.linkedin.com/in/lucyliuu/"
             />
 
             <AboutProfile
               Profile="/static/images/newFaith.png"
               Name="Faith Leung"
-              Role="Marketing Coordinator"
+              Role="Head of Marketing"
               Linkedin="https://www.linkedin.com/in/faith-leung-/"
             />
 
             <AboutProfile
               Profile="/static/images/newRebecca.png"
               Name="Rebecca Yeung"
-              Role="Marketing Coordinator"
+              Role="Head of Design"
               Linkedin="https://www.linkedin.com/in/rebecca-yeung-9bb11a316/"
             />
 
@@ -274,13 +233,6 @@ function page() {
               Name="Caleb Wu"
               Role="Front End Developer"
               Linkedin="https://www.linkedin.com/in/caleb-wu-/"
-            />
-
-            <AboutProfile
-              Profile="/static/images/newRaymond.png"
-              Name="Raymond Shen"
-              Role="Backend Coordinator"
-              Linkedin="https://www.linkedin.com/in/rayleishen/"
             />
           </div>
         </div>
